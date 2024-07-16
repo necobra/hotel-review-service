f764e6c37a0c9979d949fb559bd9270a6a547876
necobra <necobrane105@gmail.com>
2024-07-16 14:37:12 +0300

    register all models to admin panel

--- hotel_review_service/admin.py
+++ hotel_review_service/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import HotelClass, Hotel, Placement, User, Review
+
+
+admin.site.register(Hotel)
+admin.site.register(HotelClass)
+admin.site.register(Placement)
+admin.site.register(Review)
+admin.site.register(User, UserAdmin)
