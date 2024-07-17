0524e890f6c5fda646beb0185c74a69ed0b87096
necobra <necobrane105@gmail.com>
2024-07-17 09:04:21 +0300

    add model list and home views; update urls; add basic htmls for lists and home

--- hotel_review_service/views.py
+++ hotel_review_service/views.py
@@ -1,3 +1,42 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Hotel, Review
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_users = get_user_model().objects.count()
+    num_hotels = Hotel.objects.count()
+    num_reviews = Review.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_users": num_users,
+        "num_hotels": num_hotels,
+        "num_reviews": num_reviews,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "hotel_review_service/index.html", context=context)
+
+
+class HotelListView(generic.ListView):
+    model = Hotel
+    queryset = Hotel.objects.select_related("placement", "hotel_class").prefetch_related("reviews")
+    paginate_by = 5
+
+
+class ReviewListView(generic.ListView):
+    model = Review
+    queryset = Review.objects.select_related("user", "hotel")
+    paginate_by = 5
+
+
+class UserListView(generic.ListView):
+    model = get_user_model()
+    paginate_by = 5
